6612b7242deb409e07889024739755fd3c63a463
NeverLost-Never <haileyesus.mieron@gmail.com>
2023-11-29 23:37:29 -0500

    Components - Explore-Header & Listings (created)

---
+++ components/Explore-Header.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const ExploreHeader = () => {
+	return (
+		<View>
+			<Text>Explore Header</Text>
+		</View>
+	);
+};
+
+export default ExploreHeader;
+
+const styles = StyleSheet.create({});
