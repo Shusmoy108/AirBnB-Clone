3cb5e68494693f3ff0e800b873375976332b9811
NeverLost-Never <haileyesus.mieron@gmail.com>
2023-12-03 15:12:14 -0500

    pages/Wishlist - updated with Airbnb wishlist content and design

--- app/(tabs)/wishlists.tsx
+++ app/(tabs)/wishlists.tsx
@@ -1,11 +1,89 @@
-import { View, Text } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Platform,
+	SafeAreaView,
+	TouchableOpacity,
+} from "react-native";
+import { Stack } from "expo-router";
+
+// COSNTANTS
+import Colors from "@/constants/Colors";
 
 const Page = () => {
 	return (
-		<View>
-			<Text>Page</Text>
+		<View style={styles.container}>
+			<Stack.Screen
+				options={{
+					header: () => (
+						<SafeAreaView style={{ flex: 1 }}>
+							<TouchableOpacity style={styles.header}>
+								<Text style={styles.headerText}>Edit</Text>
+							</TouchableOpacity>
+						</SafeAreaView>
+					),
+				}}
+			/>
+
+			<View style={styles.title}>
+				<Text style={styles.titleText}>Wishtlists</Text>
+			</View>
+
+			<View style={styles.empty}>
+				<Text style={styles.emptyTitle}>Create your first wishlist</Text>
+
+				<Text style={styles.emptyText}>
+					As your search, tap the heart icon to save your favorite places and
+					experiences to a wishlist.
+				</Text>
+			</View>
 		</View>
 	);
 };
 
 export default Page;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: Platform.OS === "ios" ? 50 : 100,
+	},
+	header: {
+		height: Platform.OS === "ios" ? 50 : 100,
+		paddingHorizontal: 25,
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		alignItems: Platform.OS === "ios" ? "center" : "flex-end",
+	},
+	headerText: {
+		color: "#000",
+		fontSize: 20,
+		fontFamily: "mon-sb",
+		textDecorationLine: "underline",
+	},
+	title: {
+		marginTop: 20,
+		paddingHorizontal: 30,
+	},
+	titleText: {
+		color: "#000",
+		fontSize: 30,
+		fontFamily: "mon-b",
+	},
+	empty: {
+		marginTop: 45,
+		paddingHorizontal: 60,
+	},
+	emptyTitle: {
+		color: "#000",
+		fontSize: 20,
+		fontFamily: "mon-sb",
+	},
+	emptyText: {
+		marginTop: 10,
+		color: Colors.grey,
+		fontSize: 16,
+		lineHeight: 19,
+	},
+});
